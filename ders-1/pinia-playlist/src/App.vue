bff38dfe11439e211f9d7100a6a3cb37a09e5bc0
Gökhun YAYLA <50734335+gokhunyayla@users.noreply.github.com>
2023-06-10 17:06:11 +0300

    ders-1 yüklemesi

---
+++ ders-1/pinia-playlist/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  
+  <main>
+    <header>
+      <img src="@/assets/pinia-logo.svg" alt="Pinia Loog" title="Pinia Logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+  </main>
+
+</template>
